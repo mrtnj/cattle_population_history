07d41ac0d5de35c48acd584a9f3237f5da6ee458
Martin Johnsson <sorill@gmail.com>
2024-04-04 11:26:29 +0200

    Update to msprime simulation

--- python/simulate_macleod.py
+++ python/simulate_macleod.py
@@ -1,9 +1,13 @@
 
 ## Use msprime to simulate data from Macleod et al. 2013
 
+import sys
 import pandas as pd
 import numpy as np
 import msprime
+
+
+out_path = sys.argv[1]
 
 
 cattle_genome_table = pd.read_csv(
@@ -27,7 +31,7 @@ for change_ix in range(1, len(history)):
   )
 
 
-for chr_ix in range(0, 1): ##range(len(cattle_genome_table)
+for chr_ix in range(len(cattle_genome_table):
   rec_rate = cattle_genome_table.genetic_length[chr_ix]/100/cattle_genome_table.length[chr_ix]
   ts = msprime.sim_ancestry(
     samples = 20,
@@ -43,6 +47,6 @@ for chr_ix in range(0, 1): ##range(len(cattle_genome_table)
   )
   n_ind = int(ts.num_samples / 2)
   ids = [f"ind{i}" for i in range(n_ind)]
-  vcf_file = open("simulations/msprime_macleod/replicate1/" + cattle_genome_table.chr[chr_ix] + ".vcf", "w")
+  vcf_file = open(out_path + "/" + cattle_genome_table.chr[chr_ix] + ".vcf", "w")
   ts_mutated.write_vcf(vcf_file, contig_id = cattle_genome_table.chr[chr_ix], individual_names = ids)
 
