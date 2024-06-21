49fac2f9a3ba4678471592143fe1e79beab1147a
Martin Johnsson <sorill@gmail.com>
2024-06-21 15:21:19 +0200

    Combine the vcfs for the posterior simulations

--- scripts/combine_simulated_vcf.sh
+++ scripts/combine_simulated_vcf.sh
@@ -38,3 +38,43 @@ for FOLDER in simulations/decline/replicate*/ \
     --recode \
     --out $FOLDER/genotypes100k
 done
+
+
+for FOLDER in simulations/gone_posterior/gone_fjall_chip/replicate*/ \
+  simulations/gone_posterior/gone_holstein_chip/replicate*/ \
+  simulations/gone_posterior/gone_holstein_seq/replicate*/ \
+  simulations/gone_posterior/gone_jersey_seq/replicate*/ \
+  simulations/gone_posterior/gone_rodkulla_chip/replicate*/ \
+  simulations/gone_posterior/gone_srb_chip/replicate*/; do
+  grep -E "##fileformat|##source|##FILTER" \
+    $FOLDER/chr1.vcf > \
+    $FOLDER/header1.txt
+  grep -E "##FORMAT|#CHROM" \
+    $FOLDER/chr1.vcf > \
+    $FOLDER/header3.txt
+  grep "##contig" $FOLDER/chr{1..29}.vcf \
+    --no-filename > \
+    $FOLDER/header2.txt
+  cat $FOLDER/chr{1..29}.vcf | 
+    grep -v "#" > $FOLDER/genotypes_raw.vcf
+  cat $FOLDER/header1.txt \
+    $FOLDER/header2.txt \
+    $FOLDER/header3.txt \
+    $FOLDER/genotypes_raw.vcf > \
+    $FOLDER/genotypes.vcf
+  
+  plink \
+    --vcf $FOLDER/genotypes.vcf \
+    --biallelic-only \
+    --cow \
+    --recode \
+    --out $FOLDER/genotypes
+    
+  plink \
+    --vcf $FOLDER/genotypes.vcf \
+    --biallelic-only \
+    --thin-count 100000 \
+    --cow \
+    --recode \
+    --out $FOLDER/genotypes100k
+done
