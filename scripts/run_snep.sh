133162694b27d937cf4b2c89689a6c959c3dd004
Martin Johnsson <sorill@gmail.com>
2024-10-03 16:38:58 +0200

    Run on SNP chip

--- scripts/run_snep.sh
+++ scripts/run_snep.sh
@@ -1,19 +1,18 @@
 
 
-SNEP_PATH=/home/martin/tools/snep/
+SNEP_PATH=tools/snep/
 
 
-mkdir -p snep/sequence
 mkdir -p snep/snp_chip
 
-for POP in snp_chip/fjall snp_chip/fjallnara snp_chip/holstein snp_chip/bohuskulla \
-  snp_chip/ringamala snp_chip/rodkulla snp_chip/skb snp_chip/srb snp_chip/vaneko \
-  sequence/fjall sequence/rodkulla sequence/holstein sequence/jersey; do
+
+for POP in snp_chip/bohuskulla snp_chip/fjall snp_chip/fjallnara snp_chip/holstein \
+  snp_chip/ringamala snp_chip/rodkulla snp_chip/skb snp_chip/srb snp_chip/vaneko; do
 
   $SNEP_PATH/SNeP_111 \
     -ped plink/${POP}.ped \
     -out snep/$POP \
     -samplesize 2 \
     -haldane > snep/${POP}_out.txt
-  
+  ##-threads 8 \
 done
