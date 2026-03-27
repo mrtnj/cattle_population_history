3544d7a532d34cd4571d57bedc1dafa16fb32262
Martin Johnsson <sorill@gmail.com>
2026-03-27 13:30:24 +0100

    Update sample size

--- scripts/simulate_posterior_gone.sh
+++ scripts/simulate_posterior_gone.sh
@@ -6,7 +6,7 @@ set -eu
 
 HISTORIES=(gone_fjall_chip gone_holstein_chip gone_rodkulla_chip \
   gone_holstein_seq gone_jersey_seq gone_srb_chip)
-N_SAMPLES=(23 24 18 146 77 25)
+N_SAMPLES=(23 24 18 144 77 25)
 
 
 for IX in {0..5}; do
@@ -31,7 +31,7 @@ for REP in {1..10}; do
   mkdir -p simulations/macleod_posterior/replicate$REP
 
   python python/simulate_macleod.py \
-      146 \
+      144 \
       simulations/macleod_posterior/replicate$REP/
       
 done
