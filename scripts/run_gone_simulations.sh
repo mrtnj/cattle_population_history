45df02bb547c14f86b7002536e99f1c421a6a8c9
Martin Johnsson <sorill@gmail.com>
2026-03-27 13:25:42 +0100

    Add two declines scenario

--- scripts/run_gone_simulations.sh
+++ scripts/run_gone_simulations.sh
@@ -3,6 +3,7 @@
 
 
 mkdir -p gone/simulations_decline
+mkdir -p gone/simulations_two_declines
 mkdir -p gone/simulations_recovery
 mkdir -p gone/simulations_macleod
 
@@ -12,6 +13,15 @@ for REP in {1..10}; do
   bash scripts/gone_wrapper.sh \
     simulations/decline/replicate$REP/genotypes100k \
     gone/simulations_decline/replicate$REP
+
+done
+
+
+for REP in {1..10}; do
+
+  bash scripts/gone_wrapper.sh \
+    simulations/two_declines/replicate$REP/genotypes100k \
+    gone/simulations_two_declines/replicate$REP
 
 done
 
